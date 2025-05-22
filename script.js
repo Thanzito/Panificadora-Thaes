717abd45ededf6a7fff893011371d87a5451c0e1
Thanzito <163420206+Thanzito@users.noreply.github.com>
2025-05-22 12:10:00 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -20,3 +20,83 @@ function atualizarCategoria(){
         filtroCategoria.appendChild(option);
     });
 }
+
+//Atualiza a tabela com produtos filtrados
+function atualizarTabela(){
+    tabela.innerHTML = "";
+
+    const filtroTexto = busca.value.toLowerCase();
+    const categoriaSelecionada = filtroCategoria.value;
+
+    produtos.forEach((produto,index)=> {
+        const nomeOk = produto.nome.toLowerCase().includes(filtroTexto);
+        const categoriaOk = categoriaSelecionada === "" || produto.categoria === categoriaSelecionada;
+
+        if(nomeOk && categoriaOk){
+            const tr = document.createElement("tr");
+            
+            tr.innerHTML = `
+                <td>${produto.nome}</td>
+                <td>${produto.quantidade}</td>
+                <td>${produto.unidade}</td>
+                <td>${produto.categoria}</td>
+                <td>
+                    <button class="acao adicionar" onclick="alterarQuantidade(${index}, 1)">+</button>
+                    <button class="acao remover" onclick="alterarQuantidade(${index}, -1)">-</button>
+                    <button class="acao editar" onclick="editarProduto(${index}, 1)">Editar</button>
+                    <button class="acao remover" onclick="removerProduto(${index}, 1)">Remover</button>
+
+                </td>
+            `;
+                tabela.appendChild(tr);
+        }
+    })
+}
+
+//Adicionar ou editar produto
+form.addEventListener("submit",function(event){
+    event.preventDefault();
+
+    const nome = document.getElementById("nome").value;
+    const quantidade = parseInt(document.getElementById("quantidade").value);
+    const unidade = document.getElementById("unidade").value;
+    const categoria = document.getElementById("categoria").value;
+
+    const novoProduto = {nome, quantidade, unidade, categoria};
+
+    if(editandoIndex === null){
+        produtos.push(novoProduto);
+    } else {
+        produtos[editandoIndex] = novoProduto;
+        editandoIndex = null;
+    }
+    salvarDados();
+    atualizarTabela();
+    atualizarCategoria();
+    form.reset()
+});
+//Alterar quantidade
+function alterarQuantidade(index, valor){
+    produtos[index].quantidade += valor;
+    if(produtos[index].quantidade < 0){
+        produtos[index].quantidade = 0;
+    }
+    salvarDados();
+    atualizarTabela();
+}
+//Editar produto
+function editarProduto(){
+    const produto = produtos[index];
+    document.getElementById("nome").value = produto.nome;
+    document.getElementById("quantidade").value = produto.quantidade;
+    document.getElementById("unidade").value = produto.unidade;
+    document.getElementById("categoria").value = produto.categoria;
+    editandoIndex = index;
+}
+//Remover produto
+function removerProduto(){
+    produtos.splice(index, 1);
+    salvarDados()
+    atualizarCategoria();
+    atualizarTabela();
+}
